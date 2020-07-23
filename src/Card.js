687dcb7a51697ec5d6d0d833bb7235ab88e0aa31
Philippe Alphee <44515934+philalphe@users.noreply.github.com>
2020-07-23 17:44:04 -0400

    adding working files

---
+++ src/Card.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Card = ({name, email, username, id}) => {
+    //destructure
+    // const { name, email, id} = props;
+    return(
+        <div className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
+            <img alt='Robot' src ={`https://robohash.org/${id}?size=200x200`}/>
+                <div>
+                    <h2>{name}</h2>
+                    <p>{username}</p>
+                    <p>{email}</p>
+                </div>
+        </div>
+    );
+}
+
+export default Card;
