0bc7c8fe5e0f6c106e70aedc3007e5be6b07eb78
Philippe Alphee <44515934+philalphe@users.noreply.github.com>
2020-07-24 01:56:47 -0400

    adding few comments

--- src/containers/App.js
+++ src/containers/App.js
@@ -15,6 +15,7 @@ class App extends Component {
     }
 
     componentDidMount(){
+        //REST API
         fetch('https://jsonplaceholder.typicode.com/users')
             .then(response => response.json())
             .then(users =>this.setState({robots : users}));
@@ -26,6 +27,7 @@ class App extends Component {
     }
 
     render(){
+        //Destructuring
         const {robots, searchfield} = this.state;
         const filteredRobots = robots.filter(robot => {
             return robot.name.toLowerCase().includes(searchfield.toLowerCase());
